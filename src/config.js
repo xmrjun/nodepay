bbdf4ae45f99c0747ec8151f160f58039bea2420
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-14 12:32:47 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 class Config {
   constructor() {
-    this.baseURL = 'https://nodepay.org';
-    this.ipCheckURL = 'https://ipinfo.io/json';
-    this.pingURL = 'http://52.77.10.116/api/network/ping';
+    this.baseURL = "https://nodepay.org";
+    this.ipCheckURL = "https://ipinfo.io/json";
+    this.pingURL = "http://13.215.134.222/api/network/ping";
     this.retryInterval = 30000;
-    this.sessionURL = 'http://18.136.143.169/api/auth/session';
+    this.sessionURL = "http://18.136.143.169/api/auth/session";
   }
 }
 
